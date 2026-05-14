208b33bcdecc257bdac8656860efe4e0e5483e44
SreenivasVallepu <vsreenivas146@gmail.com>
2026-05-14 16:40:05 +0530

    Update hospitalB_mysqlToLanding.py

--- data/INGESTION/hospitalB_mysqlToLanding.py
+++ data/INGESTION/hospitalB_mysqlToLanding.py
@@ -12,14 +12,14 @@ bq_client = bigquery.Client()
 spark = SparkSession.builder.appName("HospitalAMySQLToLanding").getOrCreate()
 
 # Google Cloud Storage (GCS) Configuration
-GCS_BUCKET = "healthcare-bucket-22032025"
+GCS_BUCKET = "healthcare-bucket-146"
 HOSPITAL_NAME = "hospital-b"
 LANDING_PATH = f"gs://{GCS_BUCKET}/landing/{HOSPITAL_NAME}/"
 ARCHIVE_PATH = f"gs://{GCS_BUCKET}/landing/{HOSPITAL_NAME}/archive/"
 CONFIG_FILE_PATH = f"gs://{GCS_BUCKET}/configs/load_config.csv"
 
 # BigQuery Configuration
-BQ_PROJECT = "avd-databricks-demo"
+BQ_PROJECT = "project-5fd6674f-94d1-4c83-a7c
 BQ_AUDIT_TABLE = f"{BQ_PROJECT}.temp_dataset.audit_log"
 BQ_LOG_TABLE = f"{BQ_PROJECT}.temp_dataset.pipeline_logs"
 BQ_TEMP_PATH = f"{GCS_BUCKET}/temp/"  
@@ -29,7 +29,7 @@ MYSQL_CONFIG = {
     "url": "jdbc:mysql://34.59.188.6:3306/hospital_b_db?useSSL=false&allowPublicKeyRetrieval=true",
     "driver": "com.mysql.cj.jdbc.Driver",
     "user": "myuser",
-    "password": "mypass"
+    "password": "Sreenu@146"
 }
 
 ##------------------------------------------------------------------------------------------------------------------##
