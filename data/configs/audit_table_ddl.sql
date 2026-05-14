4a568f364600a24cee6cba7ca38b3c5a04696e56
SreenivasVallepu <vsreenivas146@gmail.com>
2026-05-14 16:33:20 +0530

    Update audit_table_ddl.sql

--- data/configs/audit_table_ddl.sql
+++ data/configs/audit_table_ddl.sql
@@ -1,4 +1,4 @@
-CREATE TABLE avd-databricks-demo.temp_dataset.audit_log (
+CREATE TABLE project-5fd6674f-94d1-4c83-a7c.temp_dataset.audit_log (
     data_source STRING,
     tablename STRING,
     load_type STRING,
